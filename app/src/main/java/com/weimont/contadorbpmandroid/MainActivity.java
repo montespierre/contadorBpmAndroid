9373d259cb853c293e1b848cb2f439180c68117a
unknown <montespierre@gmail.com>
2021-04-25 20:08:38 -0500

    bpm calculation

---
+++ app/src/main/java/com/weimont/contadorbpmandroid/MainActivity.java
@@ -0,0 +1,54 @@
+package com.weimont.contadorbpmandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button vBtnTap;
+    int bpm;
+    int golpe = 0;
+    long tInicial, tFinal, tTranscurrido;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // long mensaje = System.currentTimeMillis()/1000;
+
+        // Log.i("Info", Long.toString(mensaje));
+
+
+
+    }
+
+
+    public void sumarGolpe(View vista){
+        golpe = golpe + 1;
+        Log.i("Info", Integer.toString(golpe));
+
+        if(golpe == 1){
+            tInicial = System.currentTimeMillis()/1000;
+        } else{
+            tFinal = System.currentTimeMillis()/1000;
+            calcularBpm();
+        }
+    }
+
+    public void resetearBpm(View vista){
+        golpe = 0;
+        Log.i("Info", Integer.toString(golpe));
+    }
+
+    public void calcularBpm(){
+        tTranscurrido = tFinal - tInicial;
+        bpm = (int) (60 * golpe / tTranscurrido);
+        // Log.i("Info", "SEGUNDOS -> " + Integer.toString((int) tTranscurrido));
+        Log.i("Info", "BPM -> " + bpm);
+    }
+}
